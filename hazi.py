203ebfa416930d231f3d5b0aae85868ae073d134
Katona Gergő <geri.katona.mechwartsuli23@gmail.com>
2024-12-02 14:40:26 +0100

    2.1 Feladat

--- hazi.py
+++ hazi.py
@@ -21,3 +21,24 @@ while count < 5:
 print(f"Az intervallumba eső számaid: {in_interval}")
 
 print(f"Az összegük: {total_sum}")
+
+
+#2.1 Feladat
+
+szavak = ['alma', 'barack', 'Attila', 'kávé', 'szekrény', 'asztal']
+
+a_betus_szavak = []
+
+a_betu_szamlalo = 0
+
+
+for word in szavak:
+    for letter in word:
+        if letter == "a" or "A":
+            a_betu_szamlalo += 1
+    if a_betu_szamlalo > 0:
+        a_betus_szavak.append(word)
+
+
+print(f"Ennyiszer fordult elő az 'a' vagy az 'A' betű: {a_betu_szamlalo}")
+print(f"Ezekben a szavakban voltak a betűk: {a_betus_szavak}")
